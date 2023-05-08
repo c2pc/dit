75daccc7756415c8047188d6fb65b7a663c8eb4a
Чинчаров Пахрудин <chincharov@teleswyz.ru>
2023-05-08 18:57:48 +0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -6,35 +6,12 @@ import {PreRenderedAsset} from "rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/dit/",
     plugins: [vue()],
     resolve: {
         alias: {
             // @ts-ignore
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-    },
-    build: {
-        outDir: './docs',
-        rollupOptions: {
-            output: {
-                assetFileNames: (chunkInfo: PreRenderedAsset) => {
-                  if (chunkInfo.name != undefined){
-                    let extType = chunkInfo.name.split('.').at(1);
-                    if (extType != undefined && /png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                      extType = 'img';
-                    }
-                    if (extType != undefined && /woff|woff2/i.test(extType)) {
-                      extType = 'fonts';
-                    }
-                    return `assets/${extType}/[name]-[hash][extname]`;
-                  }
-                  return ''
-                },
-
-                chunkFileNames: 'assets/js/[name]-[hash].js',
-
-                entryFileNames: 'assets/js/[name]-[hash].js',
-            },
-        },
     }
 })
