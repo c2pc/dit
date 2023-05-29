d9b357ac16ed9fbfd780893a22b2569e56faf2b3
Чинчаров Пахрудин <chincharov@teleswyz.ru>
2023-05-29 21:00:53 +0300

    Background added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+    <background v-if="width < 2000"/>
     <router-view :key="route.fullPath"/>
 </template>
 
@@ -8,12 +9,15 @@ import {useRoute, useRouter} from "vue-router";
 
 // Импортируйте socket из модуля main.ts
 import {socket} from "@/main";
+import Background from "@/components/background.vue";
 
 const route = useRoute();
 const router = useRouter();
 const width = window.innerWidth
 
-window.onresize = function(){ location.reload(); }
+window.onresize = function () {
+    location.reload();
+}
 
 if (width >= 2000) {
     socket.on('pageTransition', (nextPage) => {
@@ -32,13 +36,13 @@ if (width >= 2000) {
         console.log('Изменение маршрута:', route.fullPath);
         socket.emit("pageTransition", route.fullPath);
     });
-    socket.on('pageTransition', (nextPage) => {
+    /*socket.on('pageTransition', (nextPage) => {
         console.log('Получено изменение маршрута:', nextPage);
         // Обновляем маршрут только если он отличается от текущего
         if (router.currentRoute.value.fullPath !== nextPage) {
             router.push(nextPage);
         }
-    });
+    });*/
 }
 </script>
 
